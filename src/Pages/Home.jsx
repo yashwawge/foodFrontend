8ce81ba3ddb396730dd5195c5c3c966ae7e81f08
Yash Wawge <yashwawge01@gmail.com>
2025-03-06 00:46:18 +0530

    Added home page info

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import { ArrowRightFilled } from "../Components/icons/ArrowRightFilled";
+import { IconBadgeCheck } from "../Components/icons/IconBadgeCheck";
 import PizzaImage from '../assets/images/vegies.webp';
+import cooking from '../assets/images/cooking1.jpg';
 function Home(){
     return (
         <div>
@@ -26,9 +28,50 @@ function Home(){
                 </div>
 
                <div>
-                  <img src={PizzaImage} alt="Pizza"  className="w-[400px]  md:w-[450px]  "/>
+                  <img src={PizzaImage} alt="Pizza"  className="w-[180px]  md:w-[450px]  "/>
                </div>
               
+           </section>
+
+
+           {/* services section */}
+
+           <section className="py-4 mt-6 bg-gradient-to-r from-amber-50 to-orange-300">
+                <div className="container flex flex-col md:flex-row">
+                  <div className="flex flex-col items-center justify-center rounded-lg lg:w-1/2">
+                     <img src={cooking} alt="Cooking" className="rounded-lg" width={500}/>
+                  </div>
+                  <div className="flex flex-col flex-wrap text-center lg:py-6 lg:w-1/2 lg:pl-12 lg:text-left ">
+                     <div className="flex flex-col items-center lg:items-start">
+                        <div>
+                           <h2 className="mb-2 text-5xl font-extrabold text-transparent text-font bg-gradient-to-r from-orange-600 to-orange-300 bg-clip-text">
+                              Cooked by the best <br /> cheifs in the world
+                           </h2>
+                           <p className="text-base leading-relaxed text-[#6b7280]">
+                              There are many benefits regarding to that but the main ones are: 
+                           </p>
+                        </div>   
+                     </div>
+                     <div className="w-full p-1">
+                        <div className="flex items-center h-full p-2 text-2xl rounded">
+                           <IconBadgeCheck className="text-[#f38339] w-10 h-10 mr-4"/>
+                           <span className="font-bold title-font">Perfect Taste</span>
+                        </div>
+                     </div>
+                     <div className="w-full p-1">
+                        <div className="flex items-center h-full p-2 text-2xl rounded">
+                           <IconBadgeCheck className="text-[#f38339] w-10 h-10 mr-4"/>
+                           <span className="font-bold title-font">Prepared Quickly</span>
+                        </div>
+                     </div>
+                     <div className="w-full p-1">
+                        <div className="flex items-center h-full p-2 text-2xl rounded">
+                           <IconBadgeCheck className="text-[#f38339] w-10 h-10 mr-4"/>
+                           <span className="font-bold title-font">Food hygeine guaranteed</span>
+                        </div>
+                     </div>
+                  </div>
+                </div>
            </section>
         </div>
     );
